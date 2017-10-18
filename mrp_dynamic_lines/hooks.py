1b3a2797aca81487c3cd126b88cfeff569c2e4e1
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-18 23:30:25 +1300

    [FIX] Make pre-init hook compatible with versions of pg < 9.6

--- mrp_dynamic_lines/hooks.py
+++ mrp_dynamic_lines/hooks.py
@@ -2,12 +2,13 @@
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.tools.sql import column_exists, create_column
+
 
 def pre_init_hook(cr):
-    cr.execute('ALTER TABLE IF EXISTS mrp_bom_line '
-               'ADD COLUMN IF NOT EXISTS variant_id INT')
-    cr.execute('ALTER TABLE IF EXISTS mrp_bom_line '
-               'ADD COLUMN IF NOT EXISTS product_tmpl_id INT')
+    for field in ['variant_id', 'product_tmpl_id']:
+        if not column_exists(cr, 'mrp_bom_line', field):
+            create_column(cr, 'mrp_bom_line', field, 'INT')
     cr.execute('UPDATE mrp_bom_line '
                'SET variant_id=product_id,'
                'product_tmpl_id=p.product_tmpl_id '
