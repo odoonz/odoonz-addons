68e2fea6496e4f953a4a2ba02b32c17faef39468
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-18 21:38:13 +1300

    Small refactor of invoice_sale_pricelist as it is useful to set price elsewhere than set_taxes

--- invoice_sale_pricelist/models/account_invoice_line.py
+++ invoice_sale_pricelist/models/account_invoice_line.py
@@ -11,25 +11,33 @@ class AccountInvoiceLine(models.Model):
 
     def _set_taxes(self):
         """ Used in on_change to set taxes and price."""
-        res = super(AccountInvoiceLine, self)._set_taxes()
+        super(AccountInvoiceLine, self)._set_taxes()
         if self.invoice_id.type in ('in_invoice', 'in_refund'):
-            return res
+            return
+        self.price_unit = self._get_sale_price_unit()
+
+    def _get_sale_price_unit(self):
+        product_ctx = dict(
+            quantity=self.quantity,
+            date=self.invoice_id.date_invoice,
+            uom=self.uom_id.id)
+        product_ctx.update(dict(self.env.context))
+
+        if 'pricelist' not in product_ctx:
+            pricelist = self.sale_line_ids.mapped('order_id.pricelist_id')
+
+            if len(pricelist) != 1:
+                pricelist = (
+                    self.invoice_id.partner_id.property_product_pricelist)
+            product_ctx.update({'pricelist': pricelist.id})
+
         taxes = self.product_id.taxes_id or self.account_id.tax_ids
 
         # Keep only taxes of the company
         company_id = self.company_id or self.env.user.company_id
         taxes = taxes.filtered(lambda r: r.company_id == company_id)
-        pricelist = self.sale_line_ids.mapped('order_id.pricelist_id')
 
-        if len(pricelist) != 1:
-            pricelist = self.invoice_id.partner_id.property_product_pricelist
+        product = self.product_id.with_context(**product_ctx)
 
-        product = self.product_id.with_context(
-            quantity=self.quantity,
-            date=self.invoice_id.date_invoice,
-            pricelist=pricelist.id,
-            uom=self.uom_id.id
-        )
-
-        self.price_unit = self.env['account.tax']._fix_tax_included_price(
+        return self.env['account.tax']._fix_tax_included_price(
             product.price, taxes, self.invoice_line_tax_ids)
