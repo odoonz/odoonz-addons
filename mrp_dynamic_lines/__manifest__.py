b26301d9de60151166f254cd984e7398d622c540
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-20 00:35:59 +1300

    FFS Final lint of blank lines introduced after manifest change

--- mrp_dynamic_lines/__manifest__.py
+++ mrp_dynamic_lines/__manifest__.py
@@ -6,7 +6,6 @@
     'name': 'Mrp Dynamic Lines',
     'summary': 'Dynamic BoM Transformations - ALPHA',
     'description': """
-        
         NOTE: This module is very Alpha right now after total refactor
 
         Provides a base to perform dynamic transformations to an exploded
@@ -16,9 +15,7 @@
         Match Attributes - which seeks to match the bom line with the
         attributes of the parent BoM
         Scale Weight - scales a bom line measured in kg to the quantity
-        being produced
-          
-          """,
+        being produced""",
     'version': '11.0.2.0.2',
     'license': 'AGPL-3',
     'author': 'Graeme Gellatly',
