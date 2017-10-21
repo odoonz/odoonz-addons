293f2747e25c22c66b4f6c66ce2e31412529daa8
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-22 03:31:56 +1300

    lint

--- report_helpers/__manifest__.py
+++ report_helpers/__manifest__.py
@@ -10,10 +10,5 @@
     'license': 'AGPL-3',
     'author': 'Graeme Gellatly',
     'website': 'https://o4sb.com',
-    'depends': ['base'
-    ],
-    'data': [
-    ],
-    'demo': [
-    ],
+    'depends': ['base'],
 }
