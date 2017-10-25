60f90232ec95a085a114d155856c06414a4020a0
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-26 11:32:57 +1300

    [NEW] mrp_sale_fields

---
+++ mrp_sale_fields/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Open For Small Business Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Mrp Sale Fields',
+    'description': """
+        Adds fields from sale order when linked""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Open For Small Business Ltd',
+    'website': 'https://o4sb.com',
+    'depends': ['mrp', 'sale_stock'],
+    'data': ['views/mrp_production.xml'],
+}
---
+++ mrp_sale_fields/models/mrp_production.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Open For Small Business Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MrpProduction(models.Model):
+
+    _inherit = 'mrp.production'
+
+    sale_id = fields.Many2one(
+        comodel_name='sale.order',
+        string='Sale order',
+        readonly=True,
+        related='procurement_group_id.sale_id',
+        store=True,
+    )
+    partner_id = fields.Many2one(
+        related='sale_id.partner_id',
+        string='Customer',
+        store=True,
+    )
