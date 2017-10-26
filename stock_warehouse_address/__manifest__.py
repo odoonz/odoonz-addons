893e2c4b65edfcd468f05c520b978b3edaaf5685
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-26 16:20:55 +1300

    [NEW] stock_warehouse_address

---
+++ stock_warehouse_address/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Open For Small Business Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Warehouse Address',
+    'description': """
+        Adds a postal address to warehouse""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Open For Small Business Ltd',
+    'website': 'https://o4sb.com',
+    'depends': ['stock'],
+    'data': ['views/stock_warehouse.xml'],
+}
---
+++ stock_warehouse_address/models/stock_warehouse.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Open For Small Business Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockWarehouse(models.Model):
+
+    _inherit = 'stock.warehouse'
+
+    postal_address_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Postal Address',
+    )
+    partner_id = fields.Many2one(
+        string='Physical Address',
+    )
