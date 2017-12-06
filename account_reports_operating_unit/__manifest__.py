91fbb9639d4b055f3af58f682937fe6912fb913a
Graeme Gellatly <gdgellatly@gmail.com>
2017-12-07 12:31:28 +1300

    [NEW] account_reports_operating_unit

---
+++ account_reports_operating_unit/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Open For Small Business Ltd
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Account Reports Operating Unit',
+    'description': """
+        Extends Enterprise Accounting Reports to include Operating Unit""",
+    'version': '11.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': 'Open For Small Business Ltd',
+    'website': 'https://o4sb.com',
+    'depends': ['account_reports'
+    ],
+    'data': [
+        'views/account_report.xml',
+    ],
+
+}
