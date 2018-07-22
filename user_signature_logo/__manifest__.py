308f3323e2a418dcd2f44b650042db10b6b33ac9
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-22 22:00:50 +1200

    New: user_signature_logo

---
+++ user_signature_logo/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'User Signature Logo',
+    'description': """
+        Logo for user signature""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Graeme Gellatly',
+    'website': 'https://o4sb.com',
+    'depends': [
+    ],
+    'data': [
+        'views/res_users.xml',
+    ],
+    'demo': [
+    ],
+}
