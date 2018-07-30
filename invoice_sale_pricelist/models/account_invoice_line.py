f53c8bd6efaa15ab3c425f692ab4b06f9ea26522
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 00:24:58 +1200

    Cleanup of invoice_sale_pricelist

--- invoice_sale_pricelist/models/account_invoice_line.py
+++ invoice_sale_pricelist/models/account_invoice_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Open For Small Business Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,12 +6,12 @@ from odoo import models
 
 class AccountInvoiceLine(models.Model):
 
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
     def _set_taxes(self):
         """ Used in on_change to set taxes and price."""
-        super(AccountInvoiceLine, self)._set_taxes()
-        if self.invoice_id.type in ('in_invoice', 'in_refund'):
+        super()._set_taxes()
+        if self.invoice_id.type in ("in_invoice", "in_refund"):
             return
         self.price_unit = self._get_sale_price_unit()
 
@@ -20,16 +19,18 @@ class AccountInvoiceLine(models.Model):
         product_ctx = dict(
             quantity=self.quantity,
             date=self.invoice_id.date_invoice,
-            uom=self.uom_id.id)
+            uom=self.uom_id.id,
+        )
         product_ctx.update(dict(self.env.context))
 
-        if 'pricelist' not in product_ctx:
-            pricelist = self.sale_line_ids.mapped('order_id.pricelist_id')
+        if "pricelist" not in product_ctx:
+            pricelist = self.sale_line_ids.mapped("order_id.pricelist_id")
 
             if len(pricelist) != 1:
                 pricelist = (
-                    self.invoice_id.partner_id.property_product_pricelist)
-            product_ctx.update({'pricelist': pricelist.id})
+                    self.invoice_id.partner_id.property_product_pricelist
+                )
+            product_ctx.update({"pricelist": pricelist.id})
 
         taxes = self.product_id.taxes_id or self.account_id.tax_ids
 
@@ -39,5 +40,6 @@ class AccountInvoiceLine(models.Model):
 
         product = self.product_id.with_context(**product_ctx)
 
-        return self.env['account.tax']._fix_tax_included_price(
-            product.price, taxes, self.invoice_line_tax_ids)
+        return self.env["account.tax"]._fix_tax_included_price(
+            product.price, taxes, self.invoice_line_tax_ids
+        )
