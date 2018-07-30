07a4bd054a14495295f404f664d7e99e1f985734
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 00:00:48 +1200

    Cleanup of account central billing

--- account_central_billing/models/account_invoice.py
+++ account_central_billing/models/account_invoice.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,16 +9,14 @@ class AccountInvoice(models.Model):
     """inherits account.account_invoice and adds the order_partner_id field
     as well as overriding ORM functions to ensure the parent partner and order
     partner are written and created correctly"""
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     order_partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Commercial Partner'
+        comodel_name="res.partner", string="Commercial Partner"
     )
 
     order_invoice_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Invoice Partner'
+        comodel_name="res.partner", string="Invoice Partner"
     )
 
     @api.multi
@@ -29,45 +26,56 @@ class AccountInvoice(models.Model):
         for checking the lock date
         :return: A res.partner recordset
         """
-        return super(AccountInvoice, self
-                     )._get_invoice_partner() + self.order_partner_id
+        return (
+            super()._get_invoice_partner()
+            + self.order_partner_id
+        )
 
-    @api.constrains('partner_id', 'order_partner_id')
+    @api.constrains("partner_id", "order_partner_id")
     def check_company(self):
         for record in self:
             if record.company_id.partner_id.id in [
-                    record.partner_id.id,
-                    record.partner_id.commercial_partner_id.id]:
-                raise ValidationError('Cannot self bill')
+                record.partner_id.id,
+                record.partner_id.commercial_partner_id.id,
+            ]:
+                raise ValidationError("Cannot self bill")
 
     @api.model
     def create(self, vals):
         """Function overrides create to ensure that parent account is
         always used"""
-        if vals.get('partner_id'):
-            Partner = self.env['res.partner']
-            order_partner = Partner.browse(vals['partner_id'])
+        if vals.get("partner_id"):
+            Partner = self.env["res.partner"]
+            order_partner = Partner.browse(vals["partner_id"])
             partner = order_partner.commercial_partner_id
             inv_p_id = partner.get_billing_partner(vals)
             if inv_p_id != partner.id:
-                vals.update({'partner_id': inv_p_id,
-                             'order_partner_id': partner.id,
-                             'order_invoice_id': order_partner.id})
-        return super(AccountInvoice, self).create(vals)
+                vals.update(
+                    {
+                        "partner_id": inv_p_id,
+                        "order_partner_id": partner.id,
+                        "order_invoice_id": order_partner.id,
+                    }
+                )
+        return super().create(vals)
 
     @api.multi
     def write(self, vals):
         """Function overrides create to ensure that parent account is
         always used"""
-        if vals.get('partner_id', False):
-            Partner = self.env['res.partner']
-            partner = Partner.browse(vals['partner_id']).commercial_partner_id
+        if vals.get("partner_id", False):
+            Partner = self.env["res.partner"]
+            partner = Partner.browse(vals["partner_id"]).commercial_partner_id
             inv_p_id = partner.get_billing_partner(vals, self[0])
             if inv_p_id != partner.id:
-                vals.update({'partner_id': inv_p_id,
-                             'order_partner_id': partner.id,
-                             'order_invoice_id': vals['partner_id']})
-        return super(AccountInvoice, self).write(vals)
+                vals.update(
+                    {
+                        "partner_id": inv_p_id,
+                        "order_partner_id": partner.id,
+                        "order_invoice_id": vals["partner_id"],
+                    }
+                )
+        return super().write(vals)
 
     @api.model
     def search(self, args, **kwargs):
@@ -77,13 +85,19 @@ class AccountInvoice(models.Model):
         custom filters"""
         search_args = args[:]
         for arg in search_args:
-            if arg[0] == 'partner_id' and arg[1] in [
-                    '=', 'like', 'ilike', 'child_of']:
+            if arg[0] == "partner_id" and arg[1] in [
+                "=",
+                "like",
+                "ilike",
+                "child_of",
+            ]:
                 args.remove(arg)
-                args.extend(['|', arg, ('order_partner_id', arg[1], arg[2])])
+                args.extend(["|", arg, ("order_partner_id", arg[1], arg[2])])
                 break
-        return super(AccountInvoice, self).search(args, **kwargs)
+        return super().search(args, **kwargs)
 
     def _get_refund_common_fields(self):
-        return super(AccountInvoice, self)._get_refund_common_fields() + [
-            'order_partner_id', 'order_invoice_id']
+        return super()._get_refund_common_fields() + [
+            "order_partner_id",
+            "order_invoice_id",
+        ]
--- account_central_billing/models/res_partner.py
+++ account_central_billing/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,68 +7,71 @@ from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     """inherit base.res_partner and add columns to allow central invoicing"""
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     invoicing_partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Invoicing Customer',
+        comodel_name="res.partner", string="Invoicing Customer"
     )
     store_ref = fields.Char(
-        string='Store Code',
-        help='If the customer requires specific store '
-             'references on documentation',
-        copy=False
+        string="Store Code",
+        help="If the customer requires specific store "
+        "references on documentation",
+        copy=False,
     )
     billing_partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Billing Supplier',
-        oldname='hq_partner_id'
+        comodel_name="res.partner",
+        string="Billing Supplier",
+        oldname="hq_partner_id",
     )
 
     store_ids = fields.One2many(
-        comodel_name='res.partner',
-        inverse_name='invoicing_partner_id',
-        string='Stores',
-        copy=False
+        comodel_name="res.partner",
+        inverse_name="invoicing_partner_id",
+        string="Stores",
+        copy=False,
     )
 
-    @api.constrains('store_ref', 'invoicing_partner_id')
+    @api.constrains("store_ref", "invoicing_partner_id")
     def _check_store_code(self):
         """This function checks that the store code is unique within
         the account hierarchy it belongs"""
         if not self.invoicing_partner_id:
             return True
-        for p in self:
-            store_refs = p.invoicing_partner_id.store_ids.filtered(
-                lambda r: bool(r.store_ref)).mapped('store_ref')
+        for partner in self:
+            store_refs = partner.invoicing_partner_id.store_ids.filtered(
+                lambda r: bool(r.store_ref)
+            ).mapped("store_ref")
             if len(store_refs) != len(set(store_refs)):
-                raise ValidationError('Cannot have duplicate store codes')
+                raise ValidationError("Cannot have duplicate store codes")
 
     @api.multi
     def get_billing_partner(self, vals, invoice=None):
 
         self.ensure_one()
 
-        invoice_type = vals.get('type',
-                                invoice.type if invoice else
-                                self._context.get('type', 'out_invoice'))
-        if invoice_type.startswith('out_'):
-            f = 'invoicing_partner_id'
-        elif invoice_type.startswith('in_'):
-            f = 'billing_partner_id'
+        invoice_type = vals.get(
+            "type",
+            invoice.type
+            if invoice
+            else self._context.get("type", "out_invoice"),
+        )
+        if invoice_type.startswith("out_"):
+            field = "invoicing_partner_id"
+        elif invoice_type.startswith("in_"):
+            field = "billing_partner_id"
 
-        if 'company_id' in vals:
-            invoice_company = self.env['res.company'].browse(
-                vals['company_id'])
+        if "company_id" in vals:
+            invoice_company = self.env["res.company"].browse(vals["company_id"])
         elif invoice:
             invoice_company = invoice.company_id
         else:
-            invoice_company = self.env['res.company']._company_default_get(
-                'account.invoice')
+            invoice_company = self.env["res.company"]._company_default_get(
+                "account.invoice"
+            )
         company_partner = invoice_company.partner_id
-        p = self
-        while p[f]:
-                if p[f] == company_partner:
-                    break
-                p = p[f]
-        return p.id
+        partner = self
+        while partner[field]:
+            if partner[field] == company_partner:
+                break
+            partner = partner[field]
+        return partner.id
