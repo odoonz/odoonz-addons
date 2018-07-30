c9573b595b78aec8ae550f78a77af34a6489e37f
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 00:52:56 +1200

    Cleanup mrp_sale_fields

--- mrp_sale_fields/models/mrp_production.py
+++ mrp_sale_fields/models/mrp_production.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Open For Small Business Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,17 +6,15 @@ from odoo import fields, models
 
 class MrpProduction(models.Model):
 
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     sale_id = fields.Many2one(
-        comodel_name='sale.order',
-        string='Sale order',
+        comodel_name="sale.order",
+        string="Sale order",
         readonly=True,
-        related='procurement_group_id.sale_id',
+        related="procurement_group_id.sale_id",
         store=True,
     )
     partner_id = fields.Many2one(
-        related='sale_id.partner_id',
-        string='Customer',
-        store=True,
+        related="sale_id.partner_id", string="Customer", store=True
     )
--- mrp_sale_fields/models/sale_order.py
+++ mrp_sale_fields/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,19 +6,27 @@ from odoo import api, fields, models
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     production_count = fields.Integer(
-        string='Production Orders',
-        compute='_compute_production_ids',
+        string="Production Orders", compute="_compute_production_ids"
     )
 
     @api.multi
     def _compute_production_ids(self):
-        Production = self.env['mrp.production']
+        production_data = self.env["mrp.production"].read_group(
+            domain=[("sale_id", "in", self.ids)],
+            fields=["sale_id"],
+            groupby=["sale_id"],
+        )
+        mapped_data = dict(
+            [
+                (data["sale_id"][0], data["sale_id_count"])
+                for data in production_data
+            ]
+        )
         for order in self:
-            order.production_count = len(
-                Production.search([('sale_id', '=', order.id)]))
+            order.production_count = mapped_data.get(order.id, 0)
 
     @api.multi
     def action_view_production(self):
@@ -28,6 +35,6 @@ class SaleOrder(models.Model):
         of given sales order ids.
         """
         self.ensure_one()
-        action = self.env.ref('mrp.mrp_production_action').read()[0]
-        action['domain'] = [('sale_id', '=', self.id)]
+        action = self.env.ref("mrp.mrp_production_action").read()[0]
+        action["domain"] = [("sale_id", "=", self.id)]
         return action
