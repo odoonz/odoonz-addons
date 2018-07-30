6a3b07cc22a7cbcd44fed90735a5b11eb39088d2
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 00:34:12 +1200

    Cleanup auth_oauth

--- auth_oauth_microsoft_graph/models/res_users.py
+++ auth_oauth_microsoft_graph/models/res_users.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,12 +7,13 @@ from odoo import models, api
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     @api.model
     def _auth_oauth_rpc(self, endpoint, access_token):
-        if 'graph.microsoft.com' in endpoint:
-            return requests.get(endpoint, headers={
-                'Authorization': 'Bearer {}'.format(access_token)
-            }).json()
-        return super(ResUsers, self)._auth_oauth_rpc(endpoint, access_token)
+        if "graph.microsoft.com" in endpoint:
+            return requests.get(
+                endpoint,
+                headers={"Authorization": "Bearer {}".format(access_token)},
+            ).json()
+        return super()._auth_oauth_rpc(endpoint, access_token)
