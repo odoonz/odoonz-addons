404cf2fd06d06a2ee19268d6e8b9cf9eacbe9748
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 15:48:17 +1200

    Deprecate product attribute exclusion for now - see if it is needed later

--- product_attribute_exclusion/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product_product
-from . import product_attribute_exclude_matrix
-from . import product_attribute_exclude
-from . import product_template
--- product_attribute_exclusion/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2017 Graeme Gellatly
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import test_product_attribute_exclusion
