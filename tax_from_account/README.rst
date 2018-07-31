a2979404b28dc8f1191b9c7d2ae6be1d8a4a1938
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 19:24:25 +1200

    Cleanup tax from account

---
+++ tax_from_account/README.rst
@@ -0,0 +1,57 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================
+Tax from Account
+================
+
+It is annoying to set taxes on products in a standard sales tax environment
+where nearly every product has identical tax treatment.  Its also a source of
+errors, especially in multicompany.  Here we simply use the tax assigned to the
+account if not taxes are present on the product.
+
+Installation
+============
+
+There are no special installation instructions for this module.
+
+Configuration
+=============
+
+None.
+
+Usage
+=====
+
+Assign taxes to your chart of accounts as appropriate.  If a product
+required a different configuration, then assign to a product, but otherwise
+nothing to do.
+
+Known issues / Roadmap
+======================
+
+* None at present
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/odoonz/account/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Graeme Gellatly <graeme@o4sb.com>
+
+Maintainer
+----------
+
+This module is maintained by Open for Small Business Ltd.
+
+Open for Small Business is a small developer and integrator of Odoo software since 2009.
--- tax_from_account/models/tax_from_account.py
+++ tax_from_account/models/tax_from_account.py
@@ -4,57 +4,59 @@
 from odoo import api, models
 
 
-def _get_default_taxes(line, partner=None, inv_type='out_invoice'):
-    InvoiceLine = line.env['account.invoice.line']
-    fpos = (line.order_id.fiscal_position_id or
-            line.order_id.partner_id.property_account_position_id)
+def _get_default_taxes(line, partner=None, inv_type="out_invoice"):
+    InvoiceLine = line.env["account.invoice.line"]
+    fpos = (
+        line.order_id.fiscal_position_id
+        or line.order_id.partner_id.property_account_position_id
+    )
     account = InvoiceLine.get_invoice_line_account(
-        inv_type, line.product_id, fpos, line.company_id)
-    if inv_type in ('out_invoice', 'out_refund'):
-        taxes = (
-            line.product_id.taxes_id.filtered(
-                lambda t: t.company_id == line.company_id) or
-            account.tax_ids.filtered(
-                lambda t: t.company_id == line.company_id)
-        )
-    else:
-        taxes = (
-            line.product_id.supplier_taxes_id.filtered(
-                lambda t: t.company_id == line.company_id) or
-            account.tax_ids.filtered(
-                lambda t: t.company_id == line.company_id)
-        )
-    return fpos.map_tax(
-        taxes, line.product_id, partner) if fpos else taxes
+        inv_type, line.product_id, fpos, line.company_id
+    )
+
+    tax_field = (
+        "taxes_id" if inv_type.startswith("out_") else "supplier_taxes_id"
+    )
+
+    # Don't try to collapse the filtering, needs independent evaluation of
+    # each possibility.
+    taxes = line.product_id[tax_field].filtered(
+        lambda t: t.company_id == line.company_id
+    ) or account.tax_ids.filtered(lambda t: t.company_id == line.company_id)
+
+    return fpos.map_tax(taxes, line.product_id, partner) if fpos else taxes
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
     def _compute_tax_id(self):
         for line in self:
             line.tax_id = _get_default_taxes(
-                line, line.order_id.partner_shipping_id)
+                line, line.order_id.partner_shipping_id
+            )
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
     @api.multi
     def _compute_tax_id(self):
         for line in self:
             line.taxes_id = _get_default_taxes(
-                line, line.order_id.partner_id, 'in_invoice')
+                line, line.order_id.partner_id, "in_invoice"
+            )
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
-    @api.onchange('account_id')
+    @api.onchange("account_id")
     def _onchange_account_id(self):
         super()._onchange_account_id()
         if not self.product_id and self.account_id:
             company_id = self.company_id or self.env.user.company_id
             self.invoice_line_tax_ids = self.invoice_line_tax_ids.filtered(
-                lambda r: r.company_id == company_id)
+                lambda r: r.company_id == company_id
+            )
