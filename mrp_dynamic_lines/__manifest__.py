1a383383fc3cb330b35d46c5d5c853cb43cec415
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 22:52:11 +1200

    Add README to dynamic bom

--- mrp_dynamic_lines/__manifest__.py
+++ mrp_dynamic_lines/__manifest__.py
@@ -4,17 +4,6 @@
 {
     "name": "Mrp Dynamic Lines",
     "summary": "Dynamic BoM Transformations - ALPHA",
-    "description": """
-        NOTE: This module is very Alpha right now after total refactor
-
-        Provides a base to perform dynamic transformations to an exploded
-        Bill of Materials, either during explode or raw move generation.
-
-        2 Sample transformations are included:
-        Match Attributes - which seeks to match the bom line with the
-        attributes of the parent BoM
-        Scale Weight - scales a bom line measured in kg to the quantity
-        being produced""",
     "version": "11.0.2.1.3",
     "license": "AGPL-3",
     "author": " Open for Small Business Ltd",
