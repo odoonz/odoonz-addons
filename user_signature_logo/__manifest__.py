a342a9dc963c282bea23f77588407a8fce8e85b2
Graeme Gellatly <gdgellatly@gmail.com>
2018-07-31 18:08:23 +1200

    Cleanup user_signature_logo

--- user_signature_logo/__manifest__.py
+++ user_signature_logo/__manifest__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'User Signature Logo',
-    'description': """
-        Logo for user signature""",
+    'summary': "Logo for user signature",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': ' Open for Small Business Ltd',
