41f190a84af2994a2a78da8aba9785182c2d0472
Graeme Gellatly <gdgellatly@gmail.com>
2018-08-03 21:04:03 +1200

    Tests - Stock_prodlot_qty

---
+++ stock_prodlot_qty/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_prodlot_qty
---
+++ stock_prodlot_qty/tests/test_prodlot_qty.py
@@ -0,0 +1,16 @@
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.tests import common
+
+
+class TestSaleOrder(common.TransactionCase):
+
+    def test_prodlot_qty(self):
+        lot = self.env.ref("stock.lot_icecream_0")
+        avail_qty = "(%.0f)" % lot.product_qty
+        self.assertNotIn(avail_qty, lot.name_get()[0])
+        self.assertIn(
+            avail_qty, lot.with_context(show_qty=True).name_get()[0][1]
+        )
