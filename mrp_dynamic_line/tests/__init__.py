e2414b1a65e07ea370f045eef90d5a6626b25c68
Graeme Gellatly <gdgellatly@gmail.com>
2018-08-06 16:26:32 +1200

    Lint, product tests for mrp_dynamic_line

--- mrp_dynamic_line/tests/__init__.py
+++ mrp_dynamic_line/tests/__init__.py
@@ -1,1 +1,2 @@
 from . import test_mrp_production
+from . import test_product_product
---
+++ mrp_dynamic_line/tests/test_product_product.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.tests import common
+
+
+class TestMRPProduction(common.TransactionCase):
+
+    def test_bom_count(self):
+        self.assertEquals(
+            self.env.ref("mrp_dynamic_line.manu_product").used_in_bom_count, 0
+        )
+        self.assertEquals(
+            self.env.ref("mrp_dynamic_line.raw1_product").used_in_bom_count, 1
+        )
+
+    def test_action_used_in_bom(self):
+        product = self.env.ref("mrp_dynamic_line.raw1_product")
+        action = product.action_used_in_bom()
+        self.assertIn(
+            ("bom_line_ids.product_tmpl_id", "=", product.product_tmpl_id.id),
+            action.get("domain", []),
+        )
