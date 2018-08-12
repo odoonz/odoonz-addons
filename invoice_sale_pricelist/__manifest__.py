8d607c597b23f1716c033d51a49b9bf5d69d8042
Graeme Gellatly <gdgellatly@gmail.com>
2018-08-12 20:25:50 +1200

    Add invoice_sale_pricelist tests

--- invoice_sale_pricelist/__manifest__.py
+++ invoice_sale_pricelist/__manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Invoice Sale Pricelist',
-    'summary': "Lookup prices using sale pricelist on invoices",
-    'version': '11.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Open For Small Business Ltd',
-    'website': 'https://o4sb.com',
-    'depends': ['product', 'account'],
+    "name": "Invoice Sale Pricelist",
+    "summary": "Lookup prices using sale pricelist on invoices",
+    "version": "11.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "Open For Small Business Ltd",
+    "website": "https://o4sb.com",
+    "depends": ["product", "account", "sale", "sale_stock"],
 }
