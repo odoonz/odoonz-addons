37f2d3d21a9044424bafcc3cd0fd7cb6c9ff1c3e
Graeme Gellatly <gdgellatly@gmail.com>
2018-08-16 17:40:47 +1200

    Lint

--- customer_activity_statement_monthly/__manifest__.py
+++ customer_activity_statement_monthly/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Customer Activity Statement Aged Monthly',
-    'version': '11.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'summary': ' This module enhances customer_activity_statement with option to age monthly',
+    "name": "Customer Activity Statement Aged Monthly",
+    "version": "11.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": " This module enhances customer_activity_statement with option to age monthly",
     "author": "Open For Small Business Ltd",
-    'website': 'https://o4sb.com',
-    'depends': ["customer_activity_statement"],
+    "website": "https://o4sb.com",
+    "depends": ["customer_activity_statement"],
     "data": [
-        'wizard/customer_activity_statement_wizard.xml',
-        'views/statement.xml',
-        ],
+        "wizard/customer_activity_statement_wizard.xml",
+        "views/statement.xml",
+    ],
 }
--- customer_activity_statement_monthly/wizard/customer_activity_statement_wizard.py
+++ customer_activity_statement_monthly/wizard/customer_activity_statement_wizard.py
@@ -7,20 +7,27 @@ from odoo import fields, models
 
 class CustomerActivityStatementWizard(models.TransientModel):
 
-    _inherit = 'customer.activity.statement.wizard'
+    _inherit = "customer.activity.statement.wizard"
 
-    date_start = fields.Date(default='_default_date_start')
-    date_end = fields.Date(default='_default_date_end')
+    date_start = fields.Date(default="_default_date_start")
+    date_end = fields.Date(default="_default_date_end")
 
     aging_type = fields.Selection(
-        [('days', 'Age by Days'),
-         ('months', 'Age by Months')], string='Aging Method', default='months', required=True)
+        [("days", "Age by Days"), ("months", "Age by Months")],
+        string="Aging Method",
+        default="months",
+        required=True,
+    )
 
     def _default_date_start(self):
-        return fields.Date.to_string((date.today() - timedelta(months=1)).replace(day=1))
+        return fields.Date.to_string(
+            (date.today() - timedelta(months=1)).replace(day=1)
+        )
 
     def _default_date_end(self):
-        return fields.Date.to_string(date.today().replace(day=1) - timedelta(days=1))
+        return fields.Date.to_string(
+            date.today().replace(day=1) - timedelta(days=1)
+        )
 
     def _prepare_activity_statement(self):
         result = super()._prepare_activity_statement()
