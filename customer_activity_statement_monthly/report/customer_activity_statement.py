23f28b224bb12778ce49025dc9a6c4bc83b2b3ba
Graeme Gellatly <gdgellatly@gmail.com>
2018-08-16 17:26:25 +1200

    Fix activity statement report

--- customer_activity_statement_monthly/report/customer_activity_statement.py
+++ customer_activity_statement_monthly/report/customer_activity_statement.py
@@ -8,13 +8,19 @@ from odoo import api, models
 class CustomerActivityStatement(models.AbstractModel):
     """Model of Customer Activity Statement"""
 
-    _inherit = 'report.customer_activity_statement.statement'
+    _inherit = "report.customer_activity_statement.statement"
 
     def _get_bucket_dates(self, date_end):
         if self._context.get("aging_type", "") == "months":
             res = {}
             d = date_end
-            for k in ("date_end", "minus_30", "minus_60", "minus_90", "minus_120"):
+            for k in (
+                "date_end",
+                "minus_30",
+                "minus_60",
+                "minus_90",
+                "minus_120",
+            ):
                 res[k] = d
                 d = d.replace(day=1) - timedelta(days=1)
             return res
@@ -24,6 +30,8 @@ class CustomerActivityStatement(models.AbstractModel):
     @api.multi
     def get_report_values(self, docids, data):
         aging_type = data["aging_type"]
-        self = self.with_context(aging_type=aging_type)
-        res = super().get_report_values(docids, data)
+        res = super(
+            CustomerActivityStatement, self.with_context(aging_type=aging_type)
+        ).get_report_values(docids, data)
+        res["aging_type"] = aging_type
         return res
