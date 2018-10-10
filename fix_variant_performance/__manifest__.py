46b6ee27f4086565eeff7e9361933abf06b0ef07
Graeme Gellatly <gdgellatly@gmail.com>
2018-10-11 09:42:16 +1300

    Port fix_variant_performance to v12

--- fix_variant_performance/__manifest__.py
+++ fix_variant_performance/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Fix Variant Performance",
     "description": """
         Fixes various performance issues in Odoo variant management""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
