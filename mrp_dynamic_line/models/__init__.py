a07308e6198f6f5c77f19590f7e55c29cdd7c3da
Graeme Gellatly <gdgellatly@gmail.com>
2018-12-20 13:43:49 +1300

    Fix bom count on templates

--- mrp_dynamic_line/models/__init__.py
+++ mrp_dynamic_line/models/__init__.py
@@ -3,4 +3,5 @@ from . import mrp_bom
 from . import mrp_bom_line
 from . import mrp_production
 from . import product_product
+from . import product_template
 from . import xform_substitution_map
---
+++ mrp_dynamic_line/models/product_template.py
@@ -0,0 +1,25 @@
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import api, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.multi
+    def _compute_used_in_bom_count(self):
+        for template in self:
+            template.used_in_bom_count = self.env["mrp.bom"].search_count(
+                [
+                    ("bom_line_ids.product_tmpl_id", "=", template.id),
+                ]
+            )
+
+    @api.multi
+    def action_used_in_bom(self):
+        self.ensure_one()
+        action = self.env.ref('mrp.mrp_bom_form_action').read()[0]
+        action['domain'] = [("bom_line_ids.product_tmpl_id", "=", self.id)]
+        return action
