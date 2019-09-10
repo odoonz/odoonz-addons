0a8aeb833f52988ef09abb4f819f95bffbe11211
Graeme Gellatly <gdgellatly@gmail.com>
2019-09-10 15:08:37 +1200

    Better tests for mrp_sale_fields

--- mrp_sale_fields/tests/test_sale_order.py
+++ mrp_sale_fields/tests/test_sale_order.py
@@ -2,21 +2,74 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo.tests import common
+from odoo.tests import common, Form
 
 
 class TestSaleOrder(common.TransactionCase):
     def setUp(self):
-        super().setUp()
-        self.sale = self.env.ref("sale.sale_order_1")
-        self.prod = self.env.ref("mrp.mrp_production_laptop_cust")
+        super(TestSaleOrder, self).setUp()
+        self.categ_unit = self.env.ref('uom.product_uom_categ_unit')
+        self.categ_kgm = self.env.ref('uom.product_uom_categ_kgm')
+        self.warehouse = self.env.ref('stock.warehouse0')
 
-    def test_compute_production_ids(self):
-        self.assertEqual(self.sale.production_count, 0)
-        self.prod.move_dest_ids.write({'sale_line_id': self.sale.order_line[0].id})
-        self.sale.invalidate_cache()
-        self.assertEqual(self.sale.production_count, 1)
+    def test_00_sale_mrp_flow(self):
+        """ Test sale to mrp flow with diffrent unit of measure."""
+        def create_product(name, uom_id, routes=()):
+            p = Form(self.env['product.product'])
+            p.name = name
+            p.type = 'product'
+            p.uom_id = uom_id
+            p.uom_po_id = uom_id
+            p.route_ids.clear()
+            for r in routes:
+                p.route_ids.add(r)
+            return p.save()
 
-    def test_view_production(self):
-        domain = self.sale.action_view_production().get("domain", [])
-        self.assertIn(("sale_id", "=", self.sale.id), domain)
+        self.uom_unit = self.env['uom.uom'].search([('category_id', '=', self.categ_unit.id), ('uom_type', '=', 'reference')], limit=1)
+        self.uom_unit.write({
+            'name': 'Test-Unit',
+            'rounding': 1.0})
+        self.uom_dozen = self.env['uom.uom'].create({
+            'name': 'Test-DozenA',
+            'category_id': self.categ_unit.id,
+            'factor_inv': 12,
+            'uom_type': 'bigger',
+            'rounding': 0.001})
+
+        # Create products
+        # --------------------------
+        route_manufacture = self.warehouse.manufacture_pull_id.route_id
+        route_mto = self.warehouse.mto_pull_id.route_id
+        product_a = create_product('Product A', self.uom_unit, routes=[route_manufacture, route_mto])
+        product_b = create_product('Product B', self.uom_dozen)
+
+        # ------------------------------------------------------------------------------------------
+        # Bill of materials for product A, B, D.
+        # ------------------------------------------------------------------------------------------
+
+        # Bill of materials for Product A.
+        with Form(self.env['mrp.bom']) as f:
+            f.product_tmpl_id = product_a.product_tmpl_id
+            f.product_qty = 2
+            f.product_uom_id = self.uom_dozen
+            with f.bom_line_ids.new() as line:
+                line.product_id = product_b
+                line.product_qty = 3
+                line.product_uom_id = self.uom_unit
+
+        # ----------------------------------------
+        # Create sales order of 10 Dozen product A.
+        # ----------------------------------------
+
+        order_form = Form(self.env['sale.order'])
+        order_form.partner_id = self.env.ref('base.res_partner_2')
+        with order_form.order_line.new() as line:
+            line.product_id = product_a
+            line.product_uom = self.uom_dozen
+            line.product_uom_qty = 10
+        order = order_form.save()
+        order.action_confirm()
+        self.assertEqual(order.production_count, 1)
+
+        domain = order.action_view_production().get("domain", [])
+        self.assertIn(("sale_id", "=", order.id), domain)
