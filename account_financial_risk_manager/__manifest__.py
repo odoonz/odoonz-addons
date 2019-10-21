b566126786c3c13a94ce1b1440be361dffaeb1e1
Graeme Gellatly <gdgellatly@gmail.com>
2019-10-22 12:24:09 +1300

    NEW account_financial_risk_maanger

---
+++ account_financial_risk_manager/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Financial Risk Manager',
+    'summary': """
+        Adds a seperate security group for managing credit releases""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Graeme Gellatly',
+    'website': 'https://o4sb.com',
+    'depends': ['account_financial_risk', 'account'
+    ],
+    'data': ['security/financial_risk_security.xml',
+             'views/res_partner_view.xml',
+    ],
+    'demo': [
+    ],
+}
