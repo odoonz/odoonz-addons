5b428acc3faadcb383a2f8e27146873a3b13fe29
Graeme Gellatly <gdgellatly@gmail.com>
2019-11-05 18:59:01 +1300

    FIX really dumb bug

--- mrp_sale_fields/models/__init__.py
+++ mrp_sale_fields/models/__init__.py
@@ -1,3 +1,2 @@
 from . import mrp_production
 from . import sale_order
-from . import stock_rule
--- mrp_sale_fields/models/mrp_production.py
+++ mrp_sale_fields/models/mrp_production.py
@@ -1,19 +1,36 @@
 # Copyright 2017 Open For Small Business Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class MrpProduction(models.Model):
 
     _inherit = "mrp.production"
 
+    @api.depends("move_dest_ids")
+    def _compute_sale_order(self):
+        self._cr.execute(
+            """SELECT sm.created_production_id, sol.order_id
+        FROM stock_move sm
+        LEFT JOIN sale_order_line sol ON sm.sale_line_id = sol.id
+        WHERE sm.sale_line_id IS NOT NULL
+          AND sm.created_production_id IS NOT NULL
+          AND sm.created_production_id IN %s """,
+            (tuple(self.ids),),
+        )
+        production_data = self._cr.fetchall()
+        if production_data:
+            mapped_data = dict(production_data)
+            for production in self:
+                production.sale_id = mapped_data.get(production.id, False)
+
     sale_id = fields.Many2one(
         comodel_name="sale.order",
-        string="Sale order",
+        string="Sale Order",
         readonly=True,
+        compute="_compute_sale_order",
         store=True,
-        related="procurement_group_id.sale_id",
     )
 
     partner_id = fields.Many2one(
