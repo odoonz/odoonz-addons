27ecb5caa3113dc619934ade4eff0cb9299d0b16
Graeme Gellatly <gdgellatly@gmail.com>
2019-11-26 18:40:14 +1300

    [IMP] add some changes for downstream inheritance

--- product_list_price_change/models/__init__.py
+++ product_list_price_change/models/__init__.py
@@ -3,3 +3,4 @@ from . import product_price_change_line
 from . import product_variant_price_change_line
 from . import product_price_change_implementation_delay
 from . import product_product
+from . import res_partner
---
+++ product_list_price_change/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    def _get_price_change_effective_date(self, price_change):
+        self.ensure_one()
+        return price_change.with_context(partner_id=self.commercial_partner_id.id).partner_effective_date
