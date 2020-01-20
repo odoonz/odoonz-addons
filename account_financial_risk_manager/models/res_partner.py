109801b57e380ac046c3045e97e866f3dc0280d7
Graeme Gellatly <gdgellatly@gmail.com>
2020-01-21 08:22:32 +1300

    [LINT] Greenify Account Financial Risk Manager

--- account_financial_risk_manager/models/res_partner.py
+++ account_financial_risk_manager/models/res_partner.py
@@ -1,17 +1,15 @@
-# Copyright 2016-2018 Tecnativa - Carlos Dauden
+# Copyright 2019- Graeme Gellatly <graeme@o4sb.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from datetime import datetime
-
-from dateutil.relativedelta import relativedelta
-
-from odoo import api, fields, models
+from odoo import models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def _compute_risk_allow_edit(self):
-        is_editable = self.env.user.has_group("account_financial_risk_manager.group_risk_manager")
+        is_editable = self.env.user.has_group(
+            "account_financial_risk_manager.group_risk_manager"
+        )
         for partner in self.filtered("customer"):
             partner.risk_allow_edit = is_editable
