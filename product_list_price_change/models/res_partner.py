2d8e9af88068af200620c6f5aeb2f0be48b2192b
Graeme Gellatly <gdgellatly@gmail.com>
2020-01-21 11:39:30 +1300

    [FORMAT] product_list_price_change res_partner.py

--- product_list_price_change/models/res_partner.py
+++ product_list_price_change/models/res_partner.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import models
 
 
 class ResPartner(models.Model):
 
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     def _get_price_change_effective_date(self, price_change):
         self.ensure_one()
-        return price_change.with_context(partner_id=self.commercial_partner_id.id).partner_effective_date
+        return price_change.with_context(
+            partner_id=self.commercial_partner_id.id
+        ).partner_effective_date
