441095e2903101213fcc2422d5b21bad78e72e01
Graeme Gellatly <gdgellatly@gmail.com>
2020-01-21 12:53:19 +1300

    [FIX] Unused Import

--- product_attribute_group/tests/test_attribute_group.py
+++ product_attribute_group/tests/test_attribute_group.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-from odoo.tests import Form
 
 
 class TestAttributeGroups(TransactionCase):
