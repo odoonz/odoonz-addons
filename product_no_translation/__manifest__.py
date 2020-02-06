3719fe55cb5dd812eeff5a688f6646352d620633
Graeme Gellatly <gdgellatly@gmail.com>
2020-02-07 10:53:59 +1300

    [NEW] Module to remove product translations

---
+++ product_no_translation/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product No Translation",
+    "summary": """
+        Make product related fields non translatable""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Graeme Gellatly",
+    "website": "https://o4sb.com",
+    "depends": ["product"],
+}
