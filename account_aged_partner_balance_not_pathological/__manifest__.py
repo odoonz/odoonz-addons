f5f3eaff75be4bed15c24e98b44f9ffa6f964322
Graeme Gellatly <gdgellatly@gmail.com>
2020-03-02 18:29:08 +1300

    FIX Summary Length Again

--- account_aged_partner_balance_not_pathological/__manifest__.py
+++ account_aged_partner_balance_not_pathological/__manifest__.py
@@ -3,11 +3,10 @@
 
 {
     "name": "Account Aged Partner Balance Not Pathological",
-    "summary": """
-        The default odoo implementation is optimized for tiny databases, single company,
-        without anglosaxon. This module provides constant performance across all database
-        sizes at the cost of 0.01% performance in the common case of todays date.
-        """,
+    "summary": "By default odoo optimizes for tiny, single company, "
+        "databases without anglosaxon. This module provides constant "
+        "speed across all database sizes at a cost of 0.01% "
+        "performance in the common case of todays date.",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
