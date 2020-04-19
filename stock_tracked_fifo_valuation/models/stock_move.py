ebc7f6ee014b3ccb2465e46c69862f01b909aa24
Graeme Gellatly <gdgellatly@gmail.com>
2020-04-20 00:41:49 +1200

    [UPD] Tracked FIFO with improvements to inventory costs

--- stock_tracked_fifo_valuation/models/stock_move.py
+++ stock_tracked_fifo_valuation/models/stock_move.py
@@ -52,3 +52,28 @@ class StockMove(models.Model):
                 }
             )
         super()._run_fifo(move, quantity=quantity)
+
+
+class StockInventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+
+    def _get_move_values(self, qty, location_id, location_dest_id, out):
+        """Overide of funtction
+        If we are increasing qty we need to use the fifo price for that lot
+        rather than standard_price. If the lot is already consumed it will
+        default to the oldest availalbe standard price
+        """
+        values = super()._get_move_values(qty, location_id, location_dest_id, out)
+        if (
+            self.product_qty > self.theoretical_qty
+            and self.product_id.cost_method == "fifo"
+            and self.prod_lot_id
+        ):
+            candidates = self.product_id.with_context(
+                lots={self.prod_lot_id: qty}
+            )._get_fifo_candidates_in_move_with_company(
+                move_company_id=self.inventory_id.company_id.id
+            )
+            if candidates:
+                values["price_unit"] = candidates[0].price_unit
+        return values
