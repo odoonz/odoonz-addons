34cd4e37bc2ce2a4fffd42f19571edc777a39e1f
Graeme Gellatly <gdgellatly@gmail.com>
2020-06-21 22:16:35 +1200

    Set all v13 to unported

--- account_aged_partner_balance_not_pathological/__unported__.py
+++ account_aged_partner_balance_not_pathological/__unported__.py
@@ -4,9 +4,9 @@
 {
     "name": "Account Aged Partner Balance Not Pathological",
     "summary": "By default odoo optimizes for tiny, single company, "
-        "databases without anglosaxon. This module provides constant "
-        "speed across all database sizes at a cost of 0.01% "
-        "performance in the common case of todays date.",
+    "databases without anglosaxon. This module provides constant "
+    "speed across all database sizes at a cost of 0.01% "
+    "performance in the common case of todays date.",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
