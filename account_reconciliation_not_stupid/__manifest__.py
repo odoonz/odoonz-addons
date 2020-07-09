92a60139f31084c7d2907393633403e360350e22
Jonathan Zong <jonathanz@roof.co.nz>
2020-07-09 15:59:49 +1200

    [Removed] attrs so it'll work

--- account_reconciliation_not_stupid/__manifest__.py
+++ account_reconciliation_not_stupid/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Reconciliation Not Stupid",
     "summary": """
         A not stupid proposal for reconcilable move lines""",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.1.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
