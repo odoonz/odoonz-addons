5e8ad260221a67cbda361229b9608a4e618457b7
Jonathan Zong <jonathanz@roof.co.nz>
2020-07-17 12:43:42 +1200

    [Changed] test_prepare_move_lines test to not directly test the code

--- account_reconciliation_not_stupid/tests/test_account_reconciliation.py
+++ account_reconciliation_not_stupid/tests/test_account_reconciliation.py
@@ -14,12 +14,11 @@ class TestReconciliationWidget(TestReconciliation):
         self.assertEqual(st_line_join["name"], st_line.name)
 
     def test_prepare_move_lines(self):
-        move_lines = self.env["account.move.line"].search([], limit=3)
-        move_lines_sort = move_lines.sorted(lambda r: (r.date, r.id))
+        """Move lines should be returned in ascending date order"""
+        move_lines = self.env["account.move.line"].search(
+            [], limit=3, order="date desc"
+        )
         move_lines_prepare = self.env[
             "account.reconciliation.widget"
         ]._prepare_move_lines(move_lines)
-        move_lines_sort_prepare = self.env[
-            "account.reconciliation.widget"
-        ]._prepare_move_lines(move_lines_sort)
-        self.assertEqual(move_lines_prepare, move_lines_sort_prepare)
+        self.assertEqual(move_lines[-1].id, move_lines_prepare[0]["id"])
