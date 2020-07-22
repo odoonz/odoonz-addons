c083d1a08c6d2c4305dc27bf41c8db0b8686d5ec
Jonathan Zong <jonathanz@roof.co.nz>
2020-07-23 11:10:00 +1200

    [Removed] References to api

--- mail_no_subscribe_notify/__manifest__.py
+++ mail_no_subscribe_notify/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail No Subscribe Notify",
     "summary": """
         Stops follower notifications""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
