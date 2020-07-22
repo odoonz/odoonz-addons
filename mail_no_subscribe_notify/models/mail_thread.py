f1c2b2e4746d1fe9dceaddbd3cf17aa3c1564b84
Jonathan Zong <jonathanz@roof.co.nz>
2020-07-23 11:10:00 +1200

    [Removed] References to api

--- mail_no_subscribe_notify/models/mail_thread.py
+++ mail_no_subscribe_notify/models/mail_thread.py
@@ -1,20 +1,18 @@
 # Copyright 2019 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class MailThread(models.AbstractModel):
 
     _inherit = "mail.thread"
 
-    @api.multi
     def _message_auto_subscribe(self, updated_values):
         return super(
             MailThread, self.with_context(mail_auto_subscribe_no_notify=True)
         )._message_auto_subscribe(updated_values)
 
-    @api.multi
     def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
         """Filter out automatically added partner_ids"""
         if partner_ids:
