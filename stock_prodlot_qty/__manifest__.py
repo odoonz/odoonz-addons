2ac4c29dc6dcfe6da414efb93cde5b8cda1ae36d
Jonathan Zong <jonathanz@roof.co.nz>
2020-12-11 14:32:34 +1300

    [MIG] stock_prodlot_qty to v14

--- stock_prodlot_qty/__manifest__.py
+++ stock_prodlot_qty/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Prodlot Qty",
     "summary": "Shows available qty in prodlot name",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
