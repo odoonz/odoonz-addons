3f3ec27e33d4863581561837778e491eaf12252f
Rujia Liu <rujial@roof.co.nz>
2021-01-13 12:01:45 +1300

    [MIG] Migrate mrp_sale_fields to version 14

--- mrp_sale_fields/__manifest__.py
+++ mrp_sale_fields/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mrp Sale Fields",
     "summary": "Adds fields from sale order when linked",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
