8cd79a91778012c3c57a377ef1e9ae6f07f1ff1d
Rujia Liu <rujial@roof.co.nz>
2021-01-18 11:39:19 +1300

    [MIG] product_code_builder to v14

--- product_code_builder/__manifest__.py
+++ product_code_builder/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Variant Default Code",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "O4SB",
     "contributors": [
         "OdooMRP team",
