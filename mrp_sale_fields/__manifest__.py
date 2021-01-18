6ec0f6accb029da3557037fce195fe9c9d24a306
Rujia Liu <rujial@roof.co.nz>
2021-01-18 17:53:34 +1300

    [MOD] Update the mrp_sale_fields code according to sale_mrp

--- mrp_sale_fields/__manifest__.py
+++ mrp_sale_fields/__manifest__.py
@@ -8,6 +8,6 @@
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
-    "depends": ["mrp", "sale_stock", "sale"],
+    "depends": ["sale_mrp"],
     "data": ["views/mrp_production.xml", "views/sale_order.xml"],
 }
--- mrp_sale_fields/tests/test_sale_order.py
+++ mrp_sale_fields/tests/test_sale_order.py
@@ -2,24 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo.tests import common
+from odoo.tests import Form, common
 
 
 class TestSaleOrder(common.TransactionCase):
     def setUp(self):
         super().setUp()
         self.sale = self.env.ref("sale.sale_order_1")
-        self.prod = self.env.ref("mrp.mrp_production_laptop_cust")
+        self.prod = self.env.ref("mrp.mrp_production_3")
 
     def test_compute_production_ids(self):
-        so_copy = self.sale.copy()
-        self.assertEqual(so_copy.production_count, 0)
-        self.prod.sale_id = self.sale
-        self.sale.flush()
-        self.assertEqual(self.sale.production_count, 1)
+        self.assertEqual(self.sale.mrp_production_count, 0)
+        new_order_form = Form(self.sale.copy())
+        with new_order_form.order_line.new() as line:
+            line.product_id = self.prod.product_id
+        new_order = new_order_form.save()
+        new_order.action_confirm()
+        self.assertEqual(new_order.mrp_production_count, 1)
 
     def test_view_production(self):
-        domain = self.sale.action_view_production().get("domain", [])
+        domain = self.sale.action_view_mrp_production().get("domain", [])
         self.assertIn(("sale_id", "=", self.sale.id), domain)
 
     # TODO Add test here to do full flow
