a9210fc12c882527c8b450f85c9941432544002e
RujiaL <60079589+RujiaL@users.noreply.github.com>
2021-01-19 10:21:16 +1300

    [MIG] Migrate report_helpers to v14 (#51)

--- report_helpers/__manifest__.py
+++ report_helpers/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Report Helpers",
     "summary": "Helper methods and functional fields for easier " "report generation",
-    "version": "13.0.0.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
