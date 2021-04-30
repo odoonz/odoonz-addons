bd26791ac00079a3cf6fda67b5dfb42081c78a5f
Graeme Gellatly <gdgellatly@gmail.com>
2021-04-30 13:56:44 +1200

    Bump Major Version for mail_subscribe_no_notify

--- mail_no_subscribe_notify/__manifest__.py
+++ mail_no_subscribe_notify/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail No Subscribe Notify",
     "summary": """
         Stops follower notifications""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
