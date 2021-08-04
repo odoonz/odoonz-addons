8f3eef4793eb412517fc72716043eaad7bbdc3fc
Rujia Liu <rujial@roof.co.nz>
2021-08-05 11:21:32 +1200

    [FIX] as required

--- stock_tracked_fifo_valuation/models/stock_move.py
+++ stock_tracked_fifo_valuation/models/stock_move.py
@@ -8,14 +8,17 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _action_done(self, cancel_backorder=False):
-        moves_tracked = self.filtered(
-            lambda s: s.product_id.tracking != "none" and not s._context.get("lots")
-        )
-        self -= moves_tracked
-        for move in moves_tracked:
-            move.with_context(lots=move.lot_ids.ids)._action_done(
-                cancel_backorder=cancel_backorder
+        # there is only one record has lots
+        moves_without_lots = self - self.filtered(lambda s: s._context.get("lots"))
+        if moves_without_lots:
+            moves_tracked = moves_without_lots.filtered(
+                lambda s: s.product_id.tracking != "none"
             )
+            self -= moves_tracked
+            for move in moves_tracked:
+                move.with_context(lots=move.lot_ids.ids)._action_done(
+                    cancel_backorder=cancel_backorder
+                )
         return super()._action_done(cancel_backorder=cancel_backorder)
 
     def _get_price_unit(self):
