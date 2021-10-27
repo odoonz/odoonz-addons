11e7d010eb91978a4343708e6881944b1f390f81
Graeme Gellatly <gdgellatly@gmail.com>
2021-10-27 23:55:05 +1300

    [NEW] Multicompany Operating Unit Fixes

---
+++ mc_operating_unit_fixes/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 O4SB Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Mc Operating Unit Fixes',
+    'description': """
+        Various Fixes for Using Operating Units in Multicompany""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'O4SB Ltd',
+    'website': 'https://o4sb.com',
+    'depends': [
+        "account_operating_unit",
+    ],
+
+}
---
+++ mc_operating_unit_fixes/models/account_move.py
@@ -0,0 +1,26 @@
+# Copyright 2021 O4SB Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+
+    _inherit = "account.move"
+
+    @api.model
+    def _default_operating_unit_id(self):
+        if (
+            self._context.get("default_move_type", False)
+            and self._context.get("default_move_type") != "entry"
+        ):
+            if self.env.context.get("default_journal_id"):
+                journal = self.env["account.journal"].browse(self.env.context["default_journal_id"])
+                return journal.operating_unit_id or self.env["res.users"].with_context(force_company=journal.company_id.id).operating_unit_default_get()
+            return self.env["res.users"].operating_unit_default_get()
+        return False
+
+    operating_unit_id = fields.Many2one(
+        comodel_name="operating.unit",
+        default=_default_operating_unit_id,
+    )
