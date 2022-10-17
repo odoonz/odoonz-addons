f9999ff752624628e2c09a8f63b50f68c8e7fd6d
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:43 +1300

    [MIG] product_no_translation: Migration to 16.0

--- product_no_translation/__manifest__.py
+++ product_no_translation/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product No Translation",
     "summary": """
         Make product related fields non translatable""",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
