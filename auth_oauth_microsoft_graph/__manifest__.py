cb3a36f8679d8cf36a66fb5b4051340810ed41ee
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:43 +1300

    [MIG] auth_oauth_microsoft_graph: Migration to 16.0

--- auth_oauth_microsoft_graph/__manifest__.py
+++ auth_oauth_microsoft_graph/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Microsoft Graph Oauth Authentication",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
