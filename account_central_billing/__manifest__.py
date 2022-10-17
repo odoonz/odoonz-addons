38b5613e8ab86d4b02bd6cafbb82021e651140f3
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:44 +1300

    [MIG] account_central_billing: Migration to 16.0

--- account_central_billing/__manifest__.py
+++ account_central_billing/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Central Billing",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": " Open for Small Business Ltd",
     "website": "https://o4sb.com",
