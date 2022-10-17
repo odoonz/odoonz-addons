b808e8cb2d002104ff7306ab093908a69594aeff
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:44 +1300

    [MIG] base_intracompany_user: Migration to 16.0

--- base_intracompany_user/__manifest__.py
+++ base_intracompany_user/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Intracompany User",
     "description": """
         New "Intracompany User" for executing actions restricted to a single company""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "O4SB",
     "website": "https://o4sb.com/",
