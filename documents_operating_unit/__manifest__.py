e632dc8faf7f79d66b68c8e604e5e9eed9dd16fe
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:44 +1300

    [MIG] documents_operating_unit: Migration to 16.0

--- documents_operating_unit/__manifest__.py
+++ documents_operating_unit/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Operating Unit",
     "summary": """
         Set up operating unit in documents folder""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "O4SB",
     "depends": ["documents"],
