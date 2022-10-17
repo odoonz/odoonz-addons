2d3a694ecc751bdc45e6c9c6874ab889a164e602
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:44 +1300

    [MIG] product_list_price_change: Migration to 16.0

--- product_list_price_change/__manifest__.py
+++ product_list_price_change/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product List Price Change",
     "summary": """
         Supports temporal list price changes for products""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://o4sb.com",
