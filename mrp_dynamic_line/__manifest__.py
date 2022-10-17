92181f0d08847f5383559205aff481a0287819df
Graeme Gellatly <graeme@o4sb.com>
2022-10-18 02:55:44 +1300

    [MIG] mrp_dynamic_line: Migration to 16.0

--- mrp_dynamic_line/__manifest__.py
+++ mrp_dynamic_line/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mrp Dynamic Lines",
     "summary": "Dynamic BoM Transformations",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": " Open For Small Business Ltd",
     "website": "https://o4sb.com",
--- mrp_dynamic_line/migrations/14.0.1.0.0/post-migration.py
+++
@@ -1,2 +0,0 @@
-def migrate(cr, version):
-    cr.execute("UPDATE mrp_bom_line SET product_id=variant_id;")
