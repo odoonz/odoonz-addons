9971d17140c254db9c8a946a13828334b6a3b862
Miku Laitinen <miku.laitinen@web-veistamo.fi>
2023-04-27 15:04:06 +0300

    [MIG] auth_oauth_microsoft_graph: Migration to 16.0

---
+++ auth_oauth_microsoft_graph/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Microsoft Graph Oauth Authentication",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open For Small Business Ltd",
+    "website": "https://o4sb.com",
+    "summary": "Allow users to login using Microsoft Graph.",
+    "depends": ["auth_oauth"],
+    "post_init_hook": "post_init",
+}
