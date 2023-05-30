e39c3fc2f80b5f77479286e32ad6e9e51b02288c
Danny W. Adair <danny.adair@unfold.co.nz>
2023-05-30 17:08:48 +1200

    Re-enable (all tests passed)

--- tax_from_account/__manifest__.py
+++ tax_from_account/__manifest__.py
@@ -11,5 +11,5 @@
     "author": "Open For Small Business Ltd",
     "website": "https://o4sb.com",
     "depends": ["sale", "purchase", "account", "delivery"],
-    "installable": False,
+    "installable": True,
 }
