d6bd2c04204b0c26915bda2112121d5971d2b22a
Graeme Gellatly <graeme@o4sb.com>
2024-02-18 17:46:36 +1300

    Landed Costs need to be anglosaxon

---
+++ account_anglosaxon_financial_landed_costs/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Anglosaxon Financial / Landed Costs glue module",
+    "summary": """
+        Glue module for landed costs to behave correctly""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Graeme Gellatly",
+    "website": "https://github.com/OCA/project",
+    "depends": ["account_anglosaxon_financial", "stock_landed_costs"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ account_anglosaxon_financial_landed_costs/models/account_move.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.onchange("is_landed_costs_line")
+    def _onchange_is_landed_costs_line(self):
+        res = super()._onchange_is_landed_costs_line()
+        if self.is_landed_costs_line:
+            self.move_id.anglo_saxon_financial = True
+        return res
