5fca78e678b6b28114789d5bacb7d75698c23782
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:53:02 +1200

    [MIG] mrp_sale_fields: Migration to 18.0

--- mrp_sale_fields/__manifest__.py
+++ mrp_sale_fields/__manifest__.py
@@ -4,10 +4,11 @@
 {
     "name": "Mrp Sale Fields",
     "summary": "Adds fields from sale order when linked",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://github.com/OCA/project",
     "depends": ["sale_mrp"],
     "data": ["views/mrp_production.xml", "views/mrp_workorder.xml"],
+    "installable": False,
 }
