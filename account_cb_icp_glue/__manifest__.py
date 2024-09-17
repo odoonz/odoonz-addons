ceb136a7ce07a181e84cea67028b0f1beb918390
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:44:05 +1200

    [MIG] account_cb_icp_glue: Migration to 18.0

--- account_cb_icp_glue/__manifest__.py
+++ account_cb_icp_glue/__manifest__.py
@@ -5,10 +5,11 @@
     "name": "Account Cb Icp Glue",
     "summary": """
         Glues Account Central Billing and Lock Policy""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/OCA/project",
     "depends": ["account_central_billing", "account_invoice_cutoff_policy"],
     "auto_install": True,
+    "installable": False,
 }
