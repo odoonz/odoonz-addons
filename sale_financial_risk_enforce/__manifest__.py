ee741fd27f8206334d1055fe49dbe59c374d6250
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 17:00:37 +1200

    [MIG] sale_financial_risk_enforce: Migration to 18.0

--- sale_financial_risk_enforce/__manifest__.py
+++ sale_financial_risk_enforce/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Sale Financial Risk Enforce",
     "summary": """
         Enforces credit limit is set before allowing an order to be confirmed""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly, O4SB Ltd",
     "website": "https://github.com/OCA/project",
     "depends": ["sale_financial_risk", "account_financial_risk_manager"],
     "data": ["views/res_config_settings.xml"],
-    "installable": True,
+    "installable": False,
 }
