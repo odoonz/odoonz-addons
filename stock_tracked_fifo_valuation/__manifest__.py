fe554182294b605b37f1f7d9fdcb6e1dfec1949f
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 17:03:29 +1200

    [MIG] stock_tracked_fifo_valuation: Migration to 18.0

--- stock_tracked_fifo_valuation/__manifest__.py
+++ stock_tracked_fifo_valuation/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Stock Tracked Fifo Valuation",
     "summary": """
         Selects in moves of same lot for cost tracking (more like actual cost)""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/OCA/project",
     "depends": ["stock", "stock_account"],
     "data": ["views/stock_valuation_layer.xml"],
-    "installable": True,
+    "installable": False,
 }
