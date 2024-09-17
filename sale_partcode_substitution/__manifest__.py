8521fc811de661bd05ef7ea58824e8034fdcffd5
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 17:01:15 +1200

    [MIG] sale_partcode_substitution: Migration to 18.0

--- sale_partcode_substitution/__manifest__.py
+++ sale_partcode_substitution/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Partcode Replacement",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Sales & Purchases",
     "summary": "Allows all products of a sale order to be updated by "
@@ -16,4 +16,5 @@
         "security/ir.model.access.csv",
     ],
     "demo": ["demo/product_product.xml"],
+    "installable": False,
 }
