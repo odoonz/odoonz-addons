e25daabac9f43974ad777a71e13298f2e757e67c
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:41:45 +1200

    [MIG] account_anglosaxon_financial: Migration to 18.0

--- account_anglosaxon_financial/__manifest__.py
+++ account_anglosaxon_financial/__manifest__.py
@@ -5,10 +5,11 @@
     "name": "Account Anglosaxon Financial",
     "summary": """
         Allows purely financial invoices and credits in anglosaxon environments""",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/OCA/project",
     "depends": ["account", "stock_account", "purchase_stock", "sale_stock"],
     "data": ["wizards/account_move_reversal.xml", "views/account_invoice.xml"],
+    "installable": False,
 }
