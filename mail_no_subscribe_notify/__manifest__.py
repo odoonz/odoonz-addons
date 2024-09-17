38bb4fde5c37c93c9bac958e9230a088eddaa846
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:51:54 +1200

    [MIG] mail_no_subscribe_notify: Migration to 18.0

--- mail_no_subscribe_notify/__manifest__.py
+++ mail_no_subscribe_notify/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail No Subscribe Notify",
     "summary": """
         Stops follower notifications""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/OCA/project",
