f8532cfda5c2175fba1e713a8612bbd1fa712ffe
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 17:01:48 +1200

    [MIG] sale_price_recalculation: Migration to 18.0

--- sale_price_recalculation/__manifest__.py
+++ sale_price_recalculation/__manifest__.py
@@ -5,10 +5,11 @@
     "name": "Sale Price Recalculation",
     "summary": "Allows to update the pricing on confirmed sales orders "
     "prior to invoice",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": " Open For Small Business Ltd",
     "website": "https://github.com/OCA/project",
     "depends": ["price_recalculation", "sale"],
     "data": ["wizards/sale_price_recalculation.xml", "security/ir.model.access.csv"],
+    "installable": False,
 }
