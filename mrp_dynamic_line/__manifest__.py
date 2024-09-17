6f43f0b3a05cc89ab56a1b9bec3c20d5506242d8
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:52:30 +1200

    [MIG] mrp_dynamic_line: Migration to 18.0

--- mrp_dynamic_line/__manifest__.py
+++ mrp_dynamic_line/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mrp Dynamic Lines",
     "summary": "Dynamic BoM Transformations",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": " Open For Small Business Ltd",
     "website": "https://github.com/OCA/project",
@@ -16,5 +16,5 @@
         "views/mrp_bom_line.xml",
         "views/xform_substitution_map.xml",
     ],
-    "installable": True,
+    "installable": False,
 }
