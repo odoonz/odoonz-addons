0f73f81b2d9fdce567bdfdef5a9b10d83e79c4f8
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:42:46 +1200

    [MIG] account_anglosaxon_financial_landed_costs: Migration to 18.0

--- account_anglosaxon_financial_landed_costs/__manifest__.py
+++ account_anglosaxon_financial_landed_costs/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Account Anglosaxon Financial / Landed Costs glue module",
     "summary": """
         Glue module for landed costs to behave correctly""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/OCA/project",
     "depends": ["account_anglosaxon_financial", "stock_landed_costs"],
-    "installable": True,
+    "installable": False,
     "auto_install": True,
 }
