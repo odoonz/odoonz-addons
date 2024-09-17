2c219e64087cec626ab00cb576f3983ae23f0e9d
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:49:47 +1200

    [MIG] auth_oauth_microsoft_graph: Migration to 18.0

--- auth_oauth_microsoft_graph/__manifest__.py
+++ auth_oauth_microsoft_graph/__manifest__.py
@@ -3,11 +3,12 @@
 
 {
     "name": "Microsoft Graph Oauth Authentication",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "O4SB",
     "website": "https://github.com/OCA/project",
     "summary": "Allow users to login using Microsoft Graph.",
     "depends": ["auth_oauth"],
     "post_init_hook": "post_init",
+    "installable": False,
 }
