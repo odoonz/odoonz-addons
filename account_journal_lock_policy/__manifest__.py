d8b91d729a7ee484cee0dcb19462bae20dd89e2b
Danny W. Adair <danny.adair@unfold.co.nz>
2024-09-17 16:48:31 +1200

    [MIG] account_journal_lock_policy: Migration to 18.0

--- account_journal_lock_policy/__manifest__.py
+++ account_journal_lock_policy/__manifest__.py
@@ -4,10 +4,11 @@
 {
     "name": "Accounting Journal Lock Policy",
     "summary": "Specify journal specific lock policies",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open For Small Business Ltd",
     "website": "https://github.com/OCA/project",
     "depends": ["account"],
     "data": ["views/account_journal.xml"],
+    "installable": False,
 }
