83a6ba4ff0401e28a8880c8d19da94605e0a6bd4
Graeme Gellatly <graeme@o4sb.com>
2024-10-30 08:54:20 +1300

    [WIP] stock_prodlot_qty

--- stock_prodlot_qty/models/stock_production_lot.py
+++ stock_prodlot_qty/models/stock_production_lot.py
@@ -1,17 +1,26 @@
 # Copyright 2017 MoaHub Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, api
 
 
 class StockProductionLot(models.Model):
 
     _inherit = "stock.lot"
 
-    def name_get(self):
-        res = super().name_get()
-        if self.env.context.get("show_qty") and res:
-            res = self.browse([r[0] for r in res]).mapped(
-                lambda r: (r.id, (r.name or "") + "(%.0f) " % r.product_qty)
-            )
+    @api.model
+    def _show_quantities(self):
+        return self.env.context.get("show_qty")
+
+    def _display_name_show_qty(self):
+        self.ensure_one()
+        return f"{self.display_name} ({int(self.product_qty)})"
+
+    @api.depends('name', 'quant_ids', 'quant_ids.quantity')
+    @api.depends_context('show_qty')
+    def _compute_display_name(self):
+        res = super()._compute_display_name
+        if self._show_quantities():
+            for lot in self:
+                lot.display_name = lot._display_name_show_qty
         return res
