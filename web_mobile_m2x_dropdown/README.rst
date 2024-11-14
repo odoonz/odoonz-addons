ce4560c47d386330a7c84006e740a56ef0479d51
Graeme Gellatly <graeme@o4sb.com>
2024-11-14 18:39:05 +1300

    web_mobile_m2x_dropdown

---
+++ web_mobile_m2x_dropdown/README.rst
@@ -0,0 +1,15 @@
+=======================
+Web Mobile M2x Dropdown
+=======================
+
+Opens Dropdown in mobile rather than search for m2x fields.
+
+By default mobile m2x dropdowns open the search view. I don't like
+it, my clients don't like it, and if you are reading this then at
+best you are on the fence.
+
+Install and go.
+
+
+Changelog
+=========
---
+++ web_mobile_m2x_dropdown/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Graeme Gellatly
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web Mobile M2x Dropdown",
+    "summary": """
+        Opens Dropdown in mobile rather than search for m2x fields""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Graeme Gellatly",
+    "website": "https://github.com/OCA/project",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "web_mobile_m2x_dropdown/static/src/**/*.xml",
+        ],
+    },
+}
