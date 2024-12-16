4223b69726ae666c631d4f8e0e5c26cb73d7b984
Danny W. Adair <danny.adair@unfold.nz>
2024-12-16 16:46:03 +1300

    Provide `get_related_object()` as a helper for determining email address

--- sms_clicksend/models/sms_sms.py
+++ sms_clicksend/models/sms_sms.py
@@ -9,6 +9,17 @@ class SmsSms(models.Model):
 
     error_detail = fields.Text(readonly=True)
 
+    def get_related_object(self):
+        """Return the object that the message is attached to
+
+        May be useful for get_from_email()"""
+        mail_message = self.mail_message_id
+        if mail_message and mail_message.model and mail_message.res_id:
+            related_object = self.env[mail_message.model].browse(mail_message.res_id)
+            if related_object.exists():
+                return related_object
+        return None
+
     def get_from_email(self):
         """Hook method for providing an email address for SMS replies"""
         return None
