c232b457a4b35016444dcd0f1320ed01809ed5b4
Danny W. Adair <danny.adair@unfold.nz>
2025-01-24 10:06:29 +1300

    [IMP] sms_clicksend: Prepare migration to 18.0

--- sms_clicksend/__manifest__.py
+++ sms_clicksend/__manifest__.py
@@ -5,11 +5,12 @@
     "name": "SMS ClickSend",
     "summary": """
         Use ClickSend to send SMS""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Moahub Limited",
-    "website": "https://github.com/OCA/project",
+    "website": "https://github.com/odoonz/odoonz-addons",
     "external_dependencies": {"python": ["clicksend_client"]},
     "depends": ["sms", "iap_alternative_provider"],
     "data": ["views/iap_account_view.xml", "views/sms_sms_view.xml"],
+    "installable": False,
 }
