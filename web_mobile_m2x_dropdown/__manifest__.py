aeab33a6daf312926e4ef9d7e95c68e6c948179e
Danny W. Adair <danny.adair@unfold.nz>
2025-01-24 12:10:34 +1300

    [IMP] web_mobile_m2x_dropdown: Prepare for migration to 18.0

--- web_mobile_m2x_dropdown/__manifest__.py
+++ web_mobile_m2x_dropdown/__manifest__.py
@@ -5,14 +5,15 @@
     "name": "Web Mobile M2x Dropdown",
     "summary": """
         Opens Dropdown in mobile rather than search for m2x fields""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
-    "website": "https://github.com/OCA/project",
+    "website": "https://github.com/odoonz/odoonz-addons",
     "depends": ["web"],
     "assets": {
         "web.assets_backend": [
             "web_mobile_m2x_dropdown/static/src/**/*.xml",
         ],
     },
+    "installable": False,
 }
