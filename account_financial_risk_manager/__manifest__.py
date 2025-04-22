40c0bee48d75327e249d5e16606a492ef3e7c49e
Graeme Gellatly <graeme@moahub.nz>
2025-04-22 13:12:27 +1200

    [FIX] account_financial_risk_manager: missing dependency

--- account_financial_risk_manager/__manifest__.py
+++ account_financial_risk_manager/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/odoonz/odoonz-addons",
-    "depends": ["account_financial_risk", "account"],
+    "depends": ["account_financial_risk", "account", "sale_financial_risk"],
     "data": [
         "security/financial_risk_security.xml",
         "security/ir.model.access.csv",
