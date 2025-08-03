dd0b094e22b9920c4325866fb028207e434c8045
Danny W. Adair <danny.adair@unfold.nz>
2025-08-04 10:59:24 +1200

    Add monitoring_log_requests
    Taking stewardship of this fork for v18
    * set to installable
    * adjusted authors, added pyproject.toml (to pass tests)

---
+++ monitoring_log_requests/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{
+    "name": "Monitoring: Requests Logging",
+    "version": "18.0.1.0.0",
+    "author": "Camptocamp,Numigi,Odoo Community Association (OCA),MoaHub Ltd",
+    "license": "AGPL-3",
+    "category": "category",
+    "depends": ["base", "web"],
+    "website": "https://github.com/odoonz/odoonz-addons",
+    "data": [],
+    "installable": True,
+}
---
+++ monitoring_log_requests/models/ir_http.py
@@ -0,0 +1,86 @@
+# Copyright 2016-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import json
+import logging
+import time
+
+from odoo import models
+from odoo.http import request as http_request
+from odoo.tools.config import config
+
+_logger = logging.getLogger("monitoring.http.requests")
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _dispatch(cls, endpoint):
+        begin = time.time()
+        response = super()._dispatch(endpoint)
+        end = time.time()
+        if not cls._monitoring_blacklist(http_request) and cls._monitoring_filter(
+            http_request
+        ):
+            info = cls._monitoring_info(http_request, response, begin, end)
+            cls._monitoring_log(info)
+        return response
+
+    @classmethod
+    def _monitoring_blacklist(cls, request):
+        path_info = request.httprequest.environ.get("PATH_INFO")
+        if path_info.startswith("/longpolling/"):
+            return True
+        return False
+
+    @classmethod
+    def _monitoring_filter(cls, _):
+        return True
+
+    @classmethod
+    def _monitoring_info(cls, request, response, begin, end):
+        path = request.httprequest.environ.get("PATH_INFO")
+        info = {
+            # timing
+            "start_time": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(begin)),
+            "duration": end - begin,
+            # HTTP things
+            "method": request.httprequest.method,
+            "url": request.httprequest.url,
+            "path": path,
+            "content_type": request.httprequest.environ.get("CONTENT_TYPE"),
+            "user_agent": request.httprequest.environ.get("HTTP_USER_AGENT"),
+            # Odoo things
+            "db": None,
+            "uid": request.uid,
+            "login": None,
+            "server_environment": config.get("running_env"),
+            "model": None,
+            "model_method": None,
+            "workflow_signal": None,
+            # response things
+            "response_status_code": None,
+        }
+        if hasattr(request, "status_code"):
+            info["status_code"] = response.status_code
+        if hasattr(request, "session"):
+            info.update(
+                {
+                    "login": request.session.get("login"),
+                    "db": request.session.get("db"),
+                }
+            )
+        if hasattr(request, "params"):
+            info.update(
+                {
+                    "model": request.params.get("model"),
+                    "model_method": request.params.get("method"),
+                    "workflow_signal": request.params.get("signal"),
+                }
+            )
+        return info
+
+    @classmethod
+    def _monitoring_log(cls, info):
+        _logger.info(json.dumps(info))
