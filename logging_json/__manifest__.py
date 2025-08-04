c4928cdf2a934a056285bd45aefd63168378923b
Danny W. Adair <danny.adair@unfold.nz>
2025-08-04 13:03:51 +1200

    Add logging_json
    Taking stewardship of this fork for v18
    * adjusted authors (to pass tests)

---
+++ logging_json/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "JSON Logging",
+    "version": "18.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA),MoaHub Ltd",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "base",
+    ],
+    "external_dependencies": {
+        "python": ["python-json-logger"],
+    },
+    "website": "https://github.com/odoonz/odoonz-addons",
+    "data": [],
+    "installable": True,
+}
---
+++ logging_json/json_log.py
@@ -0,0 +1,53 @@
+# Copyright 2016-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import logging
+import os
+import threading
+import uuid
+
+from odoo import http
+
+from .strtobool import strtobool
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from pythonjsonlogger.json import JsonFormatter
+except ImportError:
+    JsonFormatter = None  # noqa
+    _logger.debug("Cannot 'import pythonjsonlogger'.")
+
+
+def is_true(strval):
+    return bool(strtobool(strval or "0".lower()))
+
+
+class OdooJsonFormatter(JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        record.pid = os.getpid()
+        record.dbname = getattr(threading.current_thread(), "dbname", "?")
+        record.request_id = getattr(threading.current_thread(), "request_uuid", None)
+        record.uid = getattr(threading.current_thread(), "uid", None)
+        _super = super()
+        return _super.add_fields(log_record, record, message_dict)
+
+
+if is_true(os.environ.get("ODOO_LOGGING_JSON")):
+    formatted_message = (
+        "%(asctime)s %(pid)s %(levelname)s %(dbname)s %(name)s: %(message)s"
+    )
+    formatter = OdooJsonFormatter(formatted_message)
+    logging.getLogger().handlers[0].formatter = formatter
+
+
+# monkey patch Request constructor to store request_uuid
+org_init = http.Request.__init__
+
+
+def new_init(self, httprequest):
+    org_init(self, httprequest)
+    threading.current_thread().request_uuid = uuid.uuid4()
+
+
+http.Request.__init__ = new_init
