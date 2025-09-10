eba8f2ce0d3d7d79753c83b53cd79b9682c607a1
Graeme Gellatly <graeme@moahub.nz>
2025-09-10 18:05:45 +1200

    [FIX] account_invoice_cutoff_policy: only apply to customer invoices

--- account_invoice_cutoff_policy/models/account_invoice.py
+++ account_invoice_cutoff_policy/models/account_invoice.py
@@ -30,12 +30,12 @@ class AccountInvoice(models.Model):
                 invoice.date = invoice.invoice_date
         return super()._compute_date()
 
-    @api.constrains("invoice_date", "date", "move_type")
+    @api.constrains("invoice_date", "date", "move_type", "state")
     def _check_invoice_date(self):
         for inv in self:
-            if inv.date and inv.invoice_date and inv.date > inv.invoice_date:
-                inv.invoice_date = inv.date
-            if inv.move_type.startswith("out_"):
+            if inv.is_sale_document(include_receipts=True):
+                if inv.invoice_date and inv.invoice_date != inv.date:
+                    inv.invoice_date = inv.date
                 lock_date = inv._get_invoice_partner()._get_lock_date(inv.invoice_date)
                 if inv.invoice_date < lock_date:
                     inv.invoice_date = lock_date
