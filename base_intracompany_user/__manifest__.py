693d71f34713681bba7cd53e42d8b44042d5deb8
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] base_intracompany_user: Migration to 19.0

--- base_intracompany_user/__manifest__.py
+++ base_intracompany_user/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Intracompany User",
     "summary": """
         New "Intracompany User" for executing actions restricted to a single company""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "MoaHub Ltd",
     "website": "https://github.com/odoonz/odoonz-addons",
