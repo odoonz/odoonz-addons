c3b7b5ae107bd5a722c5e5f7dbabbcf791676720
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:19 +0000

    [MIG] account_supplier_tax_rounding: Migration to 19.0

--- account_supplier_tax_rounding/__manifest__.py
+++ account_supplier_tax_rounding/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Supplier Tax Rounding",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": " MoaHub Ltd",
     "website": "https://github.com/odoonz/odoonz-addons",
