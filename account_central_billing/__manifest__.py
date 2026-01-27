5acbc7267e80cecfbf9c84a8c9229e9074216770
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] account_central_billing: Migration to 19.0

--- account_central_billing/__manifest__.py
+++ account_central_billing/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Central Billing",
-    "version": "18.0.1.1.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": " MoaHub Ltd",
     "website": "https://github.com/odoonz/odoonz-addons",
