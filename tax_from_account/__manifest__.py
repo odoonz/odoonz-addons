ac7ca07dd05ddf1e102ce7b50eedd492df60558c
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:17 +0000

    [MIG] tax_from_account: Migration to 19.0

--- tax_from_account/__manifest__.py
+++ tax_from_account/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Sets the taxes on sales, purchases and invoices from account "
     "rather than product.  If product has taxes set, these are "
     "used instead.",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "MoaHub Ltd",
     "website": "https://github.com/odoonz/odoonz-addons",
