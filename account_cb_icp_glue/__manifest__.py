3c3f33dcb8053447d18a17101b7fa380ab6f207d
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] account_cb_icp_glue: Migration to 19.0

--- account_cb_icp_glue/__manifest__.py
+++ account_cb_icp_glue/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Cb Icp Glue",
     "summary": """
         Glues Account Central Billing and Lock Policy""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Graeme Gellatly",
     "website": "https://github.com/odoonz/odoonz-addons",
