1431396ae50ebd004f3163622ae887177cf0653e
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] sms_clicksend: Migration to 19.0

--- sms_clicksend/__manifest__.py
+++ sms_clicksend/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "SMS ClickSend",
     "summary": """
         Use ClickSend to send SMS""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "MoaHub Limited",
     "website": "https://github.com/odoonz/odoonz-addons",
