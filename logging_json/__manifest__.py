d56b2636fe624c6310a7a8c6c0d034cc375f4ccf
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] logging_json: Migration to 19.0

--- logging_json/__manifest__.py
+++ logging_json/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "JSON Logging",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA),MoaHub Ltd",
     "license": "AGPL-3",
     "category": "Extra Tools",
