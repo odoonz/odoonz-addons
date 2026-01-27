649bbc0f336fd724f591b4cf932b2e5362c2d61c
odoo-build <odoo-build@gitaggregate>
2026-01-27 22:14:18 +0000

    [MIG] stock_warehouse_address: Migration to 19.0

--- stock_warehouse_address/__manifest__.py
+++ stock_warehouse_address/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Warehouse Address",
     "summary": "Adds a postal address to warehouse",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "MoaHub Ltd",
     "website": "https://github.com/odoonz/odoonz-addons",
